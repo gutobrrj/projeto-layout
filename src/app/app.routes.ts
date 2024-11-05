39bc18fe69cc23628157bb063addc227b6f5f947
César Augusto <gutobrrj1@hotmail.com>
2024-11-05 10:25:17 -0300

    modelo de projeto com layout utilizando angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Menu1Component } from './components/menu1/menu1.component';
+import { Menu2Component } from './components/menu2/menu2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'menu1', component: Menu1Component },
+    { path: 'menu2', component: Menu2Component },
+];
